238854d5e3e1212cd684c2bf4399ee49886b4294
GonzaGomezPizarro <1912988@ucc.edu.ar>
2022-04-05 18:49:59 -0300

    .

--- main.go
+++ main.go
@@ -1,33 +1,84 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
-func encryptar(texto, url, key string) (string, error) {
-
-	_, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Error al conectar con el servidor", err)
-		return "", err
-	}
-	return texto, nil
+type RESPONSEJSON struct {
+	Result string `json:"result"`
 }
-func desencryptar(codigo, url, key string) (string, error) {
 
-	_, err := http.Get(url)
+func encryptar(texto, key string) RESPONSEJSON {
+
+	response, err := http.Post("https://classify-web.herokuapp.com/api/encrypt", "application/json", strings.NewReader(
+		`{
+			"data":"`+texto+`",
+			"key": "`+key+`"
+		}`,
+	))
 	if err != nil {
 		fmt.Println("Error al conectar con el servidor", err)
-		return "", err
+		return RESPONSEJSON{}
 	}
-	return codigo, nil
+
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error al leer la respuesta del servidor", err)
+		return RESPONSEJSON{}
+	}
+
+	var codigo RESPONSEJSON
+	err = json.Unmarshal(bytes, &codigo)
+	if err != nil {
+		fmt.Println("Error al deserializar la respuesta del servidor", err)
+		return RESPONSEJSON{}
+	}
+
+	return codigo
+}
+func desencryptar(codigo, key string) RESPONSEJSON {
+
+	response, err := http.Post("https://classify-web.herokuapp.com/api/decrypt", "application/json", strings.NewReader(
+		`{
+			"data":"`+codigo+`",
+			"key": "`+key+`"
+		}`,
+	))
+	if err != nil {
+		fmt.Println("Error al conectar con el servidor", err)
+		return RESPONSEJSON{}
+	}
+
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error al leer la respuesta del servidor", err)
+		return RESPONSEJSON{}
+	}
+
+	var texto RESPONSEJSON
+	err = json.Unmarshal(bytes, &texto)
+	if err != nil {
+		fmt.Println("Error al deserializar la respuesta del servidor", err)
+		return RESPONSEJSON{}
+	}
+
+	return texto
 }
 
 func main() {
-	url := "https://github.com/cheatsnake/classify"
 	texto := "Hola, soy una persona"
 	key := "lkm7/sjd__kf174d"
 
-	encryptar(texto, url, key)
+	fmt.Println("\n -> El texto a encriptar es:", texto)
+	fmt.Println(" -> La clave es:", key)
+
+	textoEncryptado := encryptar(texto, key)
+
+	fmt.Println("\n -> El texto encriptado es:", textoEncryptado)
+	fmt.Println("\n -> El texto desencriptado es:", desencryptar(textoEncryptado.Result, key))
+
 }
