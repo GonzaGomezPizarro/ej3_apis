dbd0848e3c5b2d4d0a98435c2b722d4b88164c0a
GonzaGomezPizarro <1912988@ucc.edu.ar>
2022-04-05 17:50:25 -0300

    .

--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
+
+func encryptar(texto, url string) (string, error) {
+
+	_, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error al conectar con el servidor", err)
+		return "", err
+	}
+	return texto, nil
+}
 
 func main() {
 	url := "https://github.com/cheatsnake/classify"
-	fmt.Println("\n\n -> La url es:", url)
+	texto := "Hola, soy una persona"
+	encryptar(texto, url)
 }
