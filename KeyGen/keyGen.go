ea6a57ca32db1f81db7b8d30ba44b4ac005bb7a8
Tomas Bucci <tomibucci@gmail.com>
2022-04-12 16:13:30 -0300

    Key Generator

---
+++ KeyGen/keyGen.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+
+	url := "https://classify-web.herokuapp.com/api/keygen?length=32?symbols=1%0A%0A"
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("", "")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
+}
