853a620d51e0292d044effecb12fed1db9d17ee4
GonzaGomezPizarro <1912988@ucc.edu.ar>
2022-04-12 16:55:05 -0300

    .

--- main.go
+++ main.go
@@ -8,11 +8,14 @@ import (
 	"strings"
 )
 
-type RESPONSEJSON struct {
+type ResponseJson struct {
 	Result string `json:"result"`
 }
+type KeyJson struct {
+	Key string `json:"key"`
+}
 
-func encryptar(texto, key string) RESPONSEJSON {
+func encryptar(texto, key string) ResponseJson {
 
 	response, err := http.Post("https://classify-web.herokuapp.com/api/encrypt", "application/json", strings.NewReader(
 		`{
@@ -22,25 +25,25 @@ func encryptar(texto, key string) RESPONSEJSON {
 	))
 	if err != nil {
 		fmt.Println("Error al conectar con el servidor", err)
-		return RESPONSEJSON{}
+		return ResponseJson{}
 	}
 
 	bytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		fmt.Println("Error al leer la respuesta del servidor", err)
-		return RESPONSEJSON{}
+		return ResponseJson{}
 	}
 
-	var codigo RESPONSEJSON
+	var codigo ResponseJson
 	err = json.Unmarshal(bytes, &codigo)
 	if err != nil {
 		fmt.Println("Error al deserializar la respuesta del servidor", err)
-		return RESPONSEJSON{}
+		return ResponseJson{}
 	}
 
 	return codigo
 }
-func desencryptar(codigo, key string) RESPONSEJSON {
+func desencryptar(codigo, key string) ResponseJson {
 
 	response, err := http.Post("https://classify-web.herokuapp.com/api/decrypt", "application/json", strings.NewReader(
 		`{
@@ -50,23 +53,44 @@ func desencryptar(codigo, key string) RESPONSEJSON {
 	))
 	if err != nil {
 		fmt.Println("Error al conectar con el servidor", err)
-		return RESPONSEJSON{}
+		return ResponseJson{}
 	}
 
 	bytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		fmt.Println("Error al leer la respuesta del servidor", err)
-		return RESPONSEJSON{}
+		return ResponseJson{}
 	}
 
-	var texto RESPONSEJSON
+	var texto ResponseJson
 	err = json.Unmarshal(bytes, &texto)
 	if err != nil {
 		fmt.Println("Error al deserializar la respuesta del servidor", err)
-		return RESPONSEJSON{}
+		return ResponseJson{}
 	}
 
 	return texto
+}
+
+func Keygen(Largo, simbolos string) KeyJson {
+	response, err := http.Get("https://classify-web.herokuapp.com/api/keygen?length=" + Largo + "&symbols=" + simbolos)
+	if err != nil {
+		fmt.Println("Error al conectar con el servidor", err)
+		return KeyJson{}
+	}
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error al leer la respuesta del servidor", err)
+		return KeyJson{}
+	}
+	var key KeyJson
+	err = json.Unmarshal(bytes, &key)
+	if err != nil {
+		fmt.Println("Error al deserializar la respuesta del servidor", err)
+		return KeyJson{}
+	}
+	return key
+
 }
 
 func main() {
@@ -80,5 +104,14 @@ func main() {
 
 	fmt.Println("\n -> El texto encriptado es:", textoEncryptado)
 	fmt.Println("\n -> El texto desencriptado es:", desencryptar(textoEncryptado.Result, key))
+
+	var Largo string
+	var simbolos string
+	fmt.Println("\n -> Ingrese el largo de la clave:")
+	fmt.Scanln(&Largo)
+	fmt.Println(" -> Con simbolos? (s=1/n=0)")
+	fmt.Scanln(&simbolos)
+
+	fmt.Println(fmt.Sprintf("\n -> La clave generada es: %s\n", Keygen(Largo, simbolos)))
 
 }
